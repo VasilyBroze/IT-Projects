4614295513149564c31a85eed26f554295d04e38
VasilyBroze <102430333+VasilyBroze@users.noreply.github.com>
2022-06-29 01:58:15 +0300

    Added DB and comments

--- Golang/TelegramBot_broker/main.go
+++ Golang/TelegramBot_broker/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type bnResp struct { //BINANCE
@@ -32,11 +33,13 @@ type yfResp struct { //YAHOO FINANCE
 	} `json:"quoteSummary"`
 }
 
-type wallet map[string]float64
-
-var db = map[int64]wallet{}
-
 func main() {
+	//СОЗДАНИЕ БД
+	database, _ := sql.Open("sqlite3", "./gopher.db")
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, chat_id INTEGER,ticker TEXT, amount FLOAT)")
+	statement.Exec()
+	//БД СОЗДАНА
 
 	bot, err := tgbotapi.NewBotAPI("5405522760:AAFqA15HEI8tn--bRzzEd-TQiobMIv2AAEo")
 	if err != nil {
@@ -56,7 +59,7 @@ func main() {
 		if update.Message == nil { // If we got a message
 			continue
 		}
-		command := strings.Split(update.Message.Text, " ")
+		command := strings.Split(strings.ToUpper(update.Message.Text), " ")
 
 		switch command[0] {
 
@@ -64,31 +67,75 @@ func main() {
 			if len(command) != 3 {
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
 			} else {
-				amount, err := strconv.ParseFloat(command[2], 64)
+				amountInput, err := strconv.ParseFloat(command[2], 64)
 				if err != nil {
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверное количество"))
 				}
-				if _, ok := db[update.Message.Chat.ID]; !ok {
-					db[update.Message.Chat.ID] = wallet{}
+
+				//СЧИТЫВАНИЕ ИЗ БАЗЫ
+				data1, _ := database.Query("SELECT chat_id, ticker, amount FROM people WHERE ticker = ? AND chat_id = ?", command[1], update.Message.Chat.ID)
+				var chatId int
+				var ticker string
+				var amount float64
+
+				data1.Next()
+				data1.Scan(&chatId, &ticker, &amount)
+				data1.Close()
+				if ticker == "" {
+					//ЕСЛИ СТРОКИ НЕТ - ДОБАВЛЕНИЕ СТРОКИ
+					statement, _ = database.Prepare("INSERT INTO people (chat_id, ticker, amount) VALUES (?, ?, ?)")
+					statement.Exec(update.Message.Chat.ID, command[1], command[2])
+					//ВЫВОД В ЧАТ
+					balanceText := fmt.Sprintf("Тикер добавлен. Баланс %v: %v", command[1], command[2])
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
+
+				} else {
+					//ЕСЛИ СТРОКА ЕСТЬ - ОБНОВЛЯЕМ ЗНАЧЕНИЕ
+					_, err := database.Exec("UPDATE people SET amount=amount + ? WHERE chat_id = ? AND ticker = ?", amountInput, update.Message.Chat.ID, command[1])
+					if err != nil {
+						fmt.Println(err)
+					}
+					//ВЫВОД В ЧАТ
+					balanceText := fmt.Sprintf("Баланс %v: %v", command[1], amount+amountInput)
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
 				}
-				db[update.Message.Chat.ID][command[1]] += amount
-				balanceText := fmt.Sprintf("Баланс %v: %v", command[1], db[update.Message.Chat.ID][command[1]])
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
 			}
 
 		case "SUB": //ОТНЯТЬ ТИКЕР
 			if len(command) != 3 {
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
 			} else {
-				amount, err := strconv.ParseFloat(command[2], 64)
+				amountInput, err := strconv.ParseFloat(command[2], 64)
 				if err != nil {
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверное количество"))
 				}
-				if _, ok := db[update.Message.Chat.ID]; !ok {
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Ошибка. Заданый тикер отсутствует."))
+
+				//СЧИТЫВАНИЕ ИЗ БАЗЫ
+				data1, _ := database.Query("SELECT chat_id,ticker, amount FROM people WHERE ticker = ? AND chat_id = ?", command[1], update.Message.Chat.ID)
+				var chatId int
+				var ticker string
+				var amount float64
+
+				data1.Next()
+				data1.Scan(&chatId, &ticker, &amount)
+				data1.Close()
+				if ticker == "" {
+					//ЕСЛИ СТРОКИ НЕТ - ДОБАВЛЕНИЕ СТРОКИ
+					statement, _ = database.Prepare("INSERT INTO people (chat_id, ticker, amount) VALUES (?, ?, ?)")
+					statement.Exec(update.Message.Chat.ID, command[1], command[2])
+					//ВЫВОД В ЧАТ
+					balanceText := fmt.Sprintf("Тикер добавлен. Баланс %v: %v", command[1], command[2])
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
+
 				} else {
-					db[update.Message.Chat.ID][command[1]] -= amount
-					balanceText := fmt.Sprintf("Баланс %v: %v", command[1], db[update.Message.Chat.ID][command[1]])
+					//ЕСЛИ СТРОКА ЕСТЬ - ОБНОВЛЯЕМ ЗНАЧЕНИЕ
+					result, err := database.Exec("UPDATE people SET amount=amount - ? WHERE chat_id = ? AND ticker = ?", amountInput, update.Message.Chat.ID, command[1])
+					if err != nil {
+						fmt.Println(err)
+						fmt.Println(result)
+					}
+					//ВЫВОД В ЧАТ
+					balanceText := fmt.Sprintf("Баланс %v: %v", command[1], amount-amountInput)
 					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
 				}
 			}
@@ -97,56 +144,74 @@ func main() {
 			if len(command) != 2 {
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
 			} else {
-
-				delete(db[update.Message.Chat.ID], command[1])
+				//ЕСЛИ СТРОКА ЕСТЬ - УДАЛЯЕМ
+				_, err := database.Exec("DELETE FROM people WHERE chat_id = ? AND ticker = ?", update.Message.Chat.ID, command[1])
+				if err != nil {
+					fmt.Println("Ошибка удаления")
+				}
+				//ВЫВОД В ЧАТ
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Тикер удалён"))
-
 			}
 
 		case "SHOW":
 			msg := ""
 			var sum float64
-			for key, value := range db[update.Message.Chat.ID] {
-				price, _ := getPrice(key)
+			rows, _ := database.Query("SELECT ticker, amount FROM people WHERE chat_id = ?", update.Message.Chat.ID)
+			var ticker string
+			var amount float64
+
+			//ПРОВЕРЯЕМ ВСЕ ДАННЫЕ В ТАБЛИЦЕ ПО ЧАТ ID
+			for rows.Next() {
+				rows.Scan(&ticker, &amount)
+				price, _ := getPrice(ticker) //BINANCE
 				if price == 0 {
-					price, _ = getPrice2(key)
+					price, _ = getPrice2(ticker) //YAHOO RUS
 					if price == 0 {
-						price, _ = getPrice3(key)
+						price, _ = getPrice3(ticker) //YAHOO
 					}
 				}
-				sum += value * price
+				sum += amount * price
 				if price != 0 {
-					msg += fmt.Sprintf("%s: %v [%.2f USD]\n", key, value, value*price)
+					msg += fmt.Sprintf("%s: %v [%.2f USD] (Цена: %.2f)\n", ticker, amount, amount*price, price)
 				} else {
-					msg += fmt.Sprintf("%s: %v [%.2f USD (Тикер не найден)]\n", key, value, value*price)
+					msg += fmt.Sprintf("%s: %v [%.2f USD (Тикер не найден)]\n", ticker, amount, amount*price)
 				}
 			}
 			msg += fmt.Sprintf("Общий балланс: %.2f USD\n", sum)
 			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
+			rows.Close()
 
 		case "SHOWRUB":
 			msg := ""
 			var sum float64
 			usd, _ := getPriceUSD()
-			for key, value := range db[update.Message.Chat.ID] {
-				price, _ := getPrice(key)
+			rows, _ := database.Query("SELECT ticker, amount FROM people WHERE chat_id = ?", update.Message.Chat.ID)
+			var ticker string
+			var amount float64
+
+			//ПРОВЕРЯЕМ ВСЕ ДАННЫЕ В ТАБЛИЦЕ ПО ЧАТ ID
+			for rows.Next() {
+				rows.Scan(&ticker, &amount)
+				price, _ := getPrice(ticker) //BINANCE
 				if price == 0 {
-					price, _ = getPrice2(key)
+					price, _ = getPrice2(ticker) //YAHOO RUS
 					if price == 0 {
-						price, _ = getPrice3(key)
+						price, _ = getPrice3(ticker) //YAHOO
 					}
 				}
-				sum += value * price * usd
+
+				sum += amount * price * usd
 				if price != 0 {
-					msg += fmt.Sprintf("%s: %v [%.2f RUB]\n", key, value, value*price*usd)
+					msg += fmt.Sprintf("%s: %v [%.2f RUB] (Цена: %.2f)\n", ticker, amount, amount*price*usd, price*usd)
 				} else {
-					msg += fmt.Sprintf("%s: %v [%.2f RUB (Тикер не найден)]\n", key, value, value*price)
+					msg += fmt.Sprintf("%s: %v [%.2f RUB (Тикер не найден)]\n", ticker, amount, amount*price)
 				}
 			}
 			msg += fmt.Sprintf("Общий балланс: %.2f RUB\n", sum)
+			//ВЫВОД В ЧАТ
 			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 
-		case "/description":
+		case "/DESCRIPTION":
 			msg := fmt.Sprintf("Описание комманд:\nADD (тикер) (количество) - добавить\nSUB (тикер) (количество) - отнять\nDEL (тикер) - удалить\nSHOW - баланс (USD)\nSHOWRUB - баланс (RUB)")
 			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
 		default:
