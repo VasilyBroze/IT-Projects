919b8a3b16b5b77e5ffd2a4674003565fcdd7d3d
VasilyBroze <102430333+VasilyBroze@users.noreply.github.com>
2022-07-18 00:55:35 +0300

    Service is Ready

---
+++ Golang/gRPC_Books_Test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	gRPC_Books_Test "gRPC_Books_Test/api/proto"
+	"gRPC_Books_Test/pkg/finder"
+
+	//"gRPC_Books_Test/pkg/finder"
+	_ "github.com/go-sql-driver/mySQL"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	s := grpc.NewServer()
+	srv := &finder.GRPCServer{}
+	gRPC_Books_Test.RegisterBooksAndAuthorsServer(s, srv)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+}
